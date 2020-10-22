737ff0b0fccdbd334fa38d1f2c26b7b3935318cc
keeks5456 <Akikogreen08@gmail.com>
2020-10-22 00:05:40 -0700

    added transition to sign up and 404 page, reformatted those pages

--- just-breath-frontend/src/component/SignUp.js
+++ just-breath-frontend/src/component/SignUp.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { signup } from '../actions/index.js';
-import { Link, withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { signup } from "../actions/index.js";
+import { Link, withRouter } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import { pageTransitions } from "../pageTransition";
 
 class Signup extends Component {
   // pretty standard
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
-      password_confirmation: '',
-      avatar: '',
-      bio: '',
-      
+      username: "",
+      password: "",
+      password_confirmation: "",
+      avatar: "",
+      bio: "",
     };
 
     this.onChange = this.onChange.bind(this);
@@ -33,20 +34,27 @@ class Signup extends Component {
     this.props.signup(this.state).then(
       (res) => {
         // route to /login once signup is complete
-        this.props.history.push('/welcome');
+        this.props.history.push("/welcome");
       },
       // if we get back a status code of >= 400 from the server...
-      (err) => { 
-        this.setState({errorMessage: err.message = 'Username or Password are Incorrect'})
-        console.log(err)
+      (err) => {
+        this.setState({
+          errorMessage: (err.message = "Username or Password are Incorrect"),
+        });
+        console.log(err);
       }
-      
     );
   }
 
   render() {
     return (
-      <div className="row">
+      <motion.div
+        initial="out"
+        animate="in"
+        exit="out"
+        variants={pageTransitions}
+        className="row"
+      >
         <div className="col-md-4 col-md-offset-4">
           <form className="form-signup" onSubmit={this.onSubmit}>
             <h1>Sign up!</h1>
@@ -90,31 +98,35 @@ class Signup extends Component {
                 onChange={this.onChange}
               />
               <div className="form-group">
-              <label htmlFor="password_confirmation">password_confirmation: </label>
-              <input
-                type="password"
-                id="password_confirmation"
-                name="password_confirmation"
-                value={this.state.password_confirmation}
-                onChange={this.onChange}
-              />
-            </div>
+                <label htmlFor="password_confirmation">
+                  password_confirmation:{" "}
+                </label>
+                <input
+                  type="password"
+                  id="password_confirmation"
+                  name="password_confirmation"
+                  value={this.state.password_confirmation}
+                  onChange={this.onChange}
+                />
+              </div>
             </div>
             <div className="form-group">
               <button className="btn btn-primary btn-lg">Sign up</button>
             </div>
-            <p>Well hello there! Already have an account!? Please login <Link to="/login">here :)</Link></p>
+            <p>
+              Well hello there! Already have an account!? Please login{" "}
+              <Link to="/login">here :)</Link>
+            </p>
           </form>
-          
         </div>
-      </div>
+      </motion.div>
     );
   }
 }
 
 // let's start adding propTypes - it's a best practice
 Signup.propTypes = {
-  signup: PropTypes.func.isRequired
+  signup: PropTypes.func.isRequired,
 };
 
-export default withRouter(connect(null, { signup })(Signup))
+export default withRouter(connect(null, { signup })(Signup));
